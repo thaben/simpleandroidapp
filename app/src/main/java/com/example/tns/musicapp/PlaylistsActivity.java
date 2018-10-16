6eae9acc83c88b4842691ee246997dd0baefb156
Tns <athbenos@gmail.com>
2018-10-16 10:18:25 +0300

    first commit

---
+++ app/src/main/java/com/example/tns/musicapp/PlaylistsActivity.java
@@ -0,0 +1,12 @@
+package com.example.tns.musicapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlaylistsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
